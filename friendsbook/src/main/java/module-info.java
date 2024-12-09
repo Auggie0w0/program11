ed8a175f112d2bd534f1e40b5c8b2ab5db1f298a
August <117049354+Auggie0w0@users.noreply.github.com>
2024-12-08 18:25:19 -0800

    delete to try again

--- friendsbook/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.friendsbook {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.friendsbook to javafx.fxml;
-    exports com.friendsbook;
-}
