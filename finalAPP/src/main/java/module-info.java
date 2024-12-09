a6610353fd0fbc9201082d5702b612ede76e33e1
August <117049354+Auggie0w0@users.noreply.github.com>
2024-12-08 20:07:28 -0800

    nope

--- finalAPP/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.finalapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.finalapp to javafx.fxml;
-    exports com.finalapp;
-}
